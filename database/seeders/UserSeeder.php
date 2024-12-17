d3c0e9f54e41aef47b76f87cb2b10c8de1937059
GGngodong <ikkoy123@gmail.com>
2024-12-17 16:28:39 +0700

    add user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'username' => 'test',
+            'email' => 'test@testmail.com',
+            'password' => Hash::make('Password@123'),
+            'token' => 'test'
+        ]);
     }
 }
