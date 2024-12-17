938e54499310db6292a0efb84f972ba1a4383e87
GGngodong <ikkoy123@gmail.com>
2024-12-17 16:28:24 +0700

    add user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,7 +17,8 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'username' => $this->username,
-            'email' => $this->email
+            'email' => $this->email,
+            'token' => $this->whenNotNull($this->token)
         ];
     }
 }
