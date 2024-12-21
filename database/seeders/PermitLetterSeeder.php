958dbd14be3a8588842880ca7c9135d4e586269a
GGngodong <ikkoy123@gmail.com>
2024-12-21 22:31:24 +0700

    tambahin seeder untuk permit leeter

--- database/seeders/PermitLetterSeeder.php
+++ database/seeders/PermitLetterSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PermitLetters;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,34 @@ class PermitLetterSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        PermitLetters::create([
+            'uraian' => 'Angkut Subang Bati',
+            'no_surat' => 'B/008/I/2023/Korp-Jkt',
+            'kategori' => 'OPS',
+            'nama_pt' => 'PT Dahana',
+            'tanggal_masuk_berkas' => '28 Februari 2023',
+            'no_produk_mabes' => 'SI/1128/I/YAN.2.10./2023',
+            'status_tahapan' => 'terverifikasi',
+        ]);
+
+        PermitLetters::create([
+            'uraian' => 'Angkut Subang Bati',
+            'no_surat' => 'B/008/I/2023/Korp-Jkt',
+            'kategori' => 'OPS',
+            'nama_pt' => 'PT Dahana',
+            'tanggal_masuk_berkas' => '28 Februari 2023',
+            'no_produk_mabes' => 'SI/1128/I/YAN.2.10./2023',
+            'status_tahapan' => 'terverifikasi',
+        ]);
+
+        PermitLetters::create([
+            'uraian' => 'Angkut Subang Bati',
+            'no_surat' => 'B/008/I/2023/Korp-Jkt',
+            'kategori' => 'OPS',
+            'nama_pt' => 'PT Dahana',
+            'tanggal_masuk_berkas' => '28 Februari 2023',
+            'no_produk_mabes' => 'SI/1128/I/YAN.2.10./2023',
+            'status_tahapan' => 'terverifikasi',
+        ]);
     }
 }
