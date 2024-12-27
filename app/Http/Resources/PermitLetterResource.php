3f3864be31ec364ebd938d2f8796b84bd20fc53c
GGngodong <ikkoy123@gmail.com>
2024-12-27 20:53:49 +0700

    updated some fields and adding date parser to tanggal

--- app/Http/Resources/PermitLetterResource.php
+++ app/Http/Resources/PermitLetterResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Helpers\DateParser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,10 +19,10 @@ class PermitLetterResource extends JsonResource
             'id' => $this->id,
             'uraian' => $this->uraian,
             'no_surat' => $this->no_surat,
-            'tanggal' => $this->tanggal_masuk_berkas ? $this->tanggal_masuk_berkas->format('d-m-Y') : null,
-            'ketegori_permit_letter' => $this->kategori,
+            'tanggal' => $this->tanggal ? \Carbon\Carbon::parse($this->tanggal)->format('d-m-Y') : null,
+            'kategori_permit_letter' => strtoupper($this->kategori_permit_letter),
             'nama_pt' => $this->nama_pt,
-            'produk_no_surat_mabes' => $this->no_produk_mabes ?? null,
+            'produk_no_surat_mabes' => $this->produk_no_surat_mabes ?? null,
             'dokumen_url' => $this->dokumen_url,
             'created_at' => $this->created_at ? $this->created_at->toDateTimeString() : null,
             'updated_at' => $this->updated_at ? $this->updated_at->toDateTimeString() : null,
