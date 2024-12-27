1dac2979b3bf242da0cb3777f7263d44d3de7580
GGngodong <ikkoy123@gmail.com>
2024-12-27 20:54:52 +0700

    adding additional data for permit letter

--- database/seeders/PermitLetterSeeder.php
+++ database/seeders/PermitLetterSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\PermitLetters;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class PermitLetterSeeder extends Seeder
 {
@@ -13,34 +13,45 @@ class PermitLetterSeeder extends Seeder
      */
     public function run(): void
     {
-        PermitLetters::create([
-            'uraian' => 'Angkut Subang Bati',
-            'no_surat' => 'B/008/I/2023/Korp-Jkt',
-            'kategori' => 'OPS',
-            'nama_pt' => 'PT Dahana',
-            'tanggal_masuk_berkas' => '28 Februari 2023',
-            'no_produk_mabes' => 'SI/1128/I/YAN.2.10./2023',
-            'status_tahapan' => 'terverifikasi',
-        ]);
+        $uniqueNoSurat = 'B/008/I/2023/Korp-Jkt' . now()->timestamp;
+        $uniqueNoProdukMabes = 'SI/1128/I/YAN.2.10./2023' . now()->timestamp;
 
         PermitLetters::create([
             'uraian' => 'Angkut Subang Bati',
-            'no_surat' => 'B/008/I/2023/Korp-Jkt',
-            'kategori' => 'OPS',
+            'no_surat' => $uniqueNoSurat,
+            'kategori_permit_letter' => 'OPS',
             'nama_pt' => 'PT Dahana',
-            'tanggal_masuk_berkas' => '28 Februari 2023',
-            'no_produk_mabes' => 'SI/1128/I/YAN.2.10./2023',
-            'status_tahapan' => 'terverifikasi',
+            'tanggal' => '2023-02-28',
+            'produk_no_surat_mabes' => $uniqueNoProdukMabes,
+//            'status_tahapan' => 'terverifikasi',
+            'dokumen' => File::get('public/dummy.pdf'),
         ]);
 
+        $uniqueNoProdukMabes2 = 'SI/381/I/YAN.2.12./2024' . now()->timestamp;
+        $uniqueNoSurat2 = '306/PHR85200/2023-SOP' . now()->timestamp;
         PermitLetters::create([
-            'uraian' => 'Angkut Subang Bati',
-            'no_surat' => 'B/008/I/2023/Korp-Jkt',
-            'kategori' => 'OPS',
-            'nama_pt' => 'PT Dahana',
-            'tanggal_masuk_berkas' => '28 Februari 2023',
-            'no_produk_mabes' => 'SI/1128/I/YAN.2.10./2023',
-            'status_tahapan' => 'terverifikasi',
+            'uraian' => 'P1 6518',
+            'no_surat' => $uniqueNoSurat2,
+            'kategori_permit_letter' => 'DTM',
+            'nama_pt' => 'PH ROKAN',
+            'tanggal' => '2024-04-01',
+            'produk_no_surat_mabes' => $uniqueNoProdukMabes2,
+//            'status_tahapan' => 'terverifikasi',
+            'dokumen' => File::get('public/dummy.pdf'),
         ]);
+
+        $uniqueNoProdukMabes3 = 'SI/230/I/YAN.2.12./2024' . now()->timestamp;
+        $uniqueNoSurat3 = '139/256/OPS-PT.PDB' . now()->timestamp;
+        PermitLetters::create([
+            'uraian' => 'P2 6797',
+            'no_surat' => $uniqueNoSurat3,
+            'kategori_permit_letter' => 'DTU',
+            'nama_pt' => 'BARAMARTA',
+            'tanggal' => '2023-02-28',
+            'produk_no_surat_mabes' => $uniqueNoProdukMabes3,
+//            'status_tahapan' => 'terverifikasi',
+            'dokumen' => File::get('public/dummy.pdf'),
+        ]);
+
     }
 }
