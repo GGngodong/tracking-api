63b9d8885d02ba5781842a274fa7d07f04e8fb38
GGngodong <ikkoy123@gmail.com>
2025-04-30 13:04:09 +0700

    code refactoring

--- routes/api.php
+++ routes/api.php
@@ -31,37 +31,37 @@ use Illuminate\Support\Facades\Route;
 //Dev Route
 
 // USERS
-Route::post('/dev/users', [UserController::class, 'register']);
-Route::post('/dev/users/login', [UserController::class, 'login']);
-Route::post('/dev/users/forgot-password', [UserController::class, 'sendPasswordResetEmail']);
+Route::post('/users', [UserController::class, 'register']);
+Route::post('/users/login', [UserController::class, 'login']);
+Route::post('/users/forgot-password', [UserController::class, 'sendPasswordResetEmail']);
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/dev/users/check-email-verified', [UserController::class, 'checkEmailVerified']);
-    Route::post('/dev/users/send-email-verification', [UserController::class, 'sendEmailVerification']);
-    Route::get('/dev/users/current', [UserController::class, 'getUser']);
-    Route::patch('/dev/users/current', [UserController::class, 'update']);
-    Route::patch('/dev/users/update-token', [UserController::class, 'updateDeviceToken']);
-    Route::delete('/dev/users/logout', [UserController::class, 'logout']);
-    Route::get('/dev/notifications', [NotificationController::class, 'index']);
-    Route::patch('/dev/notifications/{id}/read', [NotificationController::class, 'markAsRead']);
-    Route::get('/dev/notifications/{id}', [NotificationController::class, 'detailNotification']);
-    Route::delete('/dev/notifications/delete/{id}', [NotificationController::class, 'deleteNotification']);
+    Route::get('/users/check-email-verified', [UserController::class, 'checkEmailVerified']);
+    Route::post('/users/send-email-verification', [UserController::class, 'sendEmailVerification']);
+    Route::get('/users/current', [UserController::class, 'getUser']);
+    Route::patch('/users/current', [UserController::class, 'update']);
+    Route::patch('/users/update-token', [UserController::class, 'updateDeviceToken']);
+    Route::delete('/users/logout', [UserController::class, 'logout']);
+    Route::get('/notifications', [NotificationController::class, 'index']);
+    Route::patch('/notifications/{id}/read', [NotificationController::class, 'markAsRead']);
+    Route::get('/notifications/{id}', [NotificationController::class, 'detailNotification']);
+    Route::delete('/notifications/delete/{id}', [NotificationController::class, 'deleteNotification']);
     // PERMIT LETTERS
     Route::middleware(ApiPermitLetterMiddleware::class)->group(function () {
         // POST
-        Route::post('dev/permit-letters/upload', [PermitLetterController::class, 'postPermitLetter']);
+        Route::post('/permit-letters/upload', [PermitLetterController::class, 'postPermitLetter']);
         // GET
-        Route::get('dev/permit-letters/{id}', [PermitLetterController::class, 'getPermitLetterById'])->where('id', '[0-9]+');
-        Route::get('dev/permit-letters/', [PermitLetterController::class, 'getAllPermitLetter']);
-        Route::get('dev/permit-letters/rejected', [PermitLetterController::class, 'getRejectedPermitLetter']);
-        Route::get('dev/permit-letters/approved', [PermitLetterController::class, 'getApprovedPermitLetter']);
-        Route::get('dev/permit-letters/approved/{id}', [PermitLetterController::class, 'getApprovedPermitLetterById'])->where('id', '[0-9]+');
-        Route::get('dev/permit-letters/latest', [PermitLetterController::class, 'getLatestPermitLetter']);
-        Route::get('dev/permit-letters/search', [PermitLetterController::class, 'searchPermitLetter']);
-        Route::get('dev/permit-letters/pending', [PermitLetterController::class, 'getPendingPermitLetter']);
+        Route::get('/permit-letters/{id}', [PermitLetterController::class, 'getPermitLetterById'])->where('id', '[0-9]+');
+        Route::get('/permit-letters/', [PermitLetterController::class, 'getAllPermitLetter']);
+        Route::get('/permit-letters/rejected', [PermitLetterController::class, 'getRejectedPermitLetter']);
+        Route::get('/permit-letters/approved', [PermitLetterController::class, 'getApprovedPermitLetter']);
+        Route::get('/permit-letters/approved/{id}', [PermitLetterController::class, 'getApprovedPermitLetterById'])->where('id', '[0-9]+');
+        Route::get('/permit-letters/latest', [PermitLetterController::class, 'getLatestPermitLetter']);
+        Route::get('/permit-letters/search', [PermitLetterController::class, 'searchPermitLetter']);
+        Route::get('/permit-letters/pending', [PermitLetterController::class, 'getPendingPermitLetter']);
         // PATCH
-        Route::patch('dev/permit-letters/edit/{id}', [PermitLetterController::class, 'updatePermitLetter']);
+        Route::patch('/permit-letters/edit/{id}', [PermitLetterController::class, 'updatePermitLetter']);
         // DELETE
-        Route::delete('dev/permit-letters/delete/{id}', [PermitLetterController::class, 'deletePermitLetter']);
+        Route::delete('/permit-letters/delete/{id}', [PermitLetterController::class, 'deletePermitLetter']);
     });
 });
 
